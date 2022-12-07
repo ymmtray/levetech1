dc7592caec150eddff8fe9992f628cb282ef09e5
ymmtray <ymmt.ray0821@gmail.com>
2022-12-07 14:45:25 +0900

    ADD 記事作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
+
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
     
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts/create');
     }
 }
