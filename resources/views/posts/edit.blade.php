6755caa872a3683368cd91c9e48d21b47acc775b
ymmtray <ymmt.ray0821@gmail.com>
2022-12-12 22:56:52 +0900

    ブログ管理

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            @method('POT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="update"/>
+        </form>
+        <div class='footer'>
+            <a href="/posts/{{ $post->id }}">戻る</a>
+        </div>
+    </body>
+</html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,6 +18,9 @@
                 
             </div>
         </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
+        </div>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
